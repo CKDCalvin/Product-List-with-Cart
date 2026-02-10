2dc6e26b9dbd00390fff4934e74089da2dc6b233
CKDCalvin <calvinkugonza@gmail.com>
2026-02-09 21:40:23 -0600

    implemented some changes to the js file

--- function.js
+++ function.js
@@ -1,10 +1,13 @@
 const productList = document.getElementById('product-list');
+const cartCount = document.getElementById('cart-count');
+const cartList = document.getElementById('cart-list');
 
 let items = [];
+let cart = [];
 
+//
 function displayProducts(products) {
     productList.innerHTML = '';
-
 
     products.forEach(product => {
         const extDiv = document.createElement('div');
@@ -13,7 +16,7 @@ function displayProducts(products) {
         extDiv.innerHTML = `
         <div class="product-image">
             <img src="${product.image.desktop}" alt="${product.name}" class="product-img" />
-            <button onclick="addToCart(${product.id})">Add to Cart</button>
+            <button onclick="" class="cart-btn">Add to Cart</button>
         </div>        
             <p>${product.category}</p>
             <h3>${product.name}</h3>
@@ -21,14 +24,14 @@ function displayProducts(products) {
         `;
 
         productList.appendChild(extDiv);
-
-        // const productItem = document.createElement('li');
-        // productItem.textContent = `${product.name} - $${product.price}`;
-        // productList.appendChild(productItem);
     })
-    loadData();
+
+    cartList.innerHTML = cartItems.map(item => `<li>${item.name} - $${item.price}</li>`).join('');
+    cartCount.textContent = cartItems.length;
+
 }
 
+// feth data from json file
 async function loadData() {
     try {
         const response = await fetch('data.json');
@@ -46,6 +49,39 @@ function renderProducts() {
     loadData().then(products => {
         displayProducts(products);
     });
+
+}
+renderProducts();
+
+function addToCart(name, price) {
+    const item = items.find(product => product.name === name);
+    if (item) {
+        item.quantity = item.quantity++;
+    }
+    else {
+        cart.push({ name, price: parseFloat(price), quantity: 1 });
+    }
+
+    updateCart();
 }
 
-renderProducts();
+
+function updateCart() {
+    const cartItems = cart.map(item => `<li>${item.name} - $${item.price} x ${item.quantity}</li>`).join('');
+    cartList.innerHTML = cartItems;
+    cartCount.textContent = cart.length;
+}
+
+
+document.querySelectorAll('.cart-btn').forEach(button => {
+    button.addEventListener('click', (event) => {
+        const productElement = event.target.closest('.product-item');
+        const productName = productElement.querySelector('h3').textContent;
+        const product = items.find(item => item.name === productName);
+        if (product) {
+            cart.push(product);
+            updateCartCount();
+            displayCartItems();
+        }
+    });
+});
